b17974a3c0c33cf11088f27669b478375596443f
Olivia Cantwell <oliviacantwell1@gmail.com>
2022-04-09 20:55:38 -0600

    fghjkl

--- final/js/contact.js
+++ final/js/contact.js
@@ -3,3 +3,7 @@ const hambutton = document.querySelector('.hamburger');
 const mainnav = document.querySelector('.navigation');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+function thanksPage() {
+    window.location.href="https://oliviacantwell.github.io/wdd230/final/thanks.html";
+}
